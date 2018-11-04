fedeadbd9c144ab6d5363fea16a3d120fbf23f9c
Tiffany Carter <tafby88@gmail.com>
2018-11-04 22:42:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -101,7 +101,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     programmers_hash.update(programmers_hash){ , value| value * 2 }
+     programmers_hash[:known_for] = "GUI"
 end
 
 def adding_to_dennis
