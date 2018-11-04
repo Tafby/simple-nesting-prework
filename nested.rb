f3de8f03b8d095ac968eb960287dafe11b6e6a23
Tiffany Carter <tafby88@gmail.com>
2018-11-04 22:35:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,23 +60,26 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+# add the following information to the top level of 
 
-    progammer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP, C"]})
+programmer_hash = 
+{
+       :grace_hopper => {
+         :known_for => "COBOL",
+         :languages => ["COBOL", "FORTRAN"]
+       },
+       :alan_kay => {
+         :known_for => "Object Orientation",
+         :languages => ["Smalltalk", "LISP"]
+       },
+       :dennis_ritchie => {
+         :known_for => "Unix",
+         :languages => ["C"]
+       }
+    }
+    
+programmer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
+
 end
 
 def changing_alan
